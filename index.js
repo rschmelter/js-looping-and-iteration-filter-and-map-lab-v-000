bb22fa4253ebd52e2d8651eea8b802cf403132a4
Ryan Schmelter <rschmelter33@gmail.com>
2018-08-12 18:11:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,14 @@
 function driversWithRevenueOver (drivers, revenue) {
   return drivers.filter(function (driver) {
     return driver.revenue > revenue;
+
+  });
+}
+
+function driverNamesWithRevenueOver(driver, revenue) {
+  return driversWithRevenueOver(drivers, revenue).map(function (driver) {
+    return driver.name;
     
-  })
-
-
+  });
+  
 }
